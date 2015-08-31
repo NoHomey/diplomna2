232563ef01ee5d17a4b9d9fd1413cafac3932926
NoHomey <ivo_stratev@abv.bg>
2015-08-31 13:50:57 +0300

    finished tutorial {http://cwbuecheler.com/web/tutorials/2014/restful-web-app-node-express-mongodb/}

--- diplomna2/nodetest2/public/javascripts/global.js
+++ diplomna2/nodetest2/public/javascripts/global.js
@@ -1,7 +1,6 @@
+    var userListData = [];
     document.body.onload = populateTable;
     document.getElementById("btnAddUser").addEventListener("click", addUser);
-
-    var userListData = [];
 
     Array.prototype.first = function () {
         return this[0];
@@ -19,7 +18,7 @@
     function populateTable(event) {
         if(event) event.preventDefault();
         AJAX(index, 'GET', '/users/userlist', '');
-    };
+    }
 
     function index (event) {
             event.preventDefault();
@@ -33,7 +32,7 @@
                         var tr = newElement("tr");
                         tr.appendChild(newElement("td")).appendChild(newElement("a", {href : "#", class : "linkshowuser", rel : element.username, innerHTML : element.username, onclick : showUserInfo, onclickArguments : [element]}));
                         tr.appendChild(newElement("td", {innerHTML : element.email}));
-                        tr.appendChild(newElement("td")).appendChild(newElement("a", {href : "#", class : "linkshowuser", rel : element._id, innerHTML : "delete", onclick : showUserInfo,onclickArguments : [element]}));
+                        tr.appendChild(newElement("td")).appendChild(newElement("a", {href : "#", class : "linkshowuser", rel : element._id, innerHTML : "delete", onclick : deleteUser,onclickArguments : [element]}));
                         document.getElementById("tbody").appendChild(tr);
                     });
                   } else 
@@ -79,19 +78,47 @@
                 'location': document.getElementById("inputUserLocation").value,
                 'gender': document.getElementById("inputUserGender").value
             };
-            AJAX(addNewUser, 'POST', '/users/adduser', JSON.stringify(newUser)) ;
-
+            AJAX(addNewUser, 'POST', '/users/adduser', JSON.stringify(newUser));
         } else alert('Please fill in all fields');
-
     }
 
     function addNewUser (event) {
-        var res = JSON.parse(event.target.responseText);
-        if(res.msg === '') {
-            var inputs = document.getElementById("addUser").getElementsByTagName("input");
-            for(input in inputs)
-                if(inputs[input].value !== undefined)
-                    inputs[input].value = "";
-            populateTable(null);
-        } else alert('Error: ' + res.msg)
+        try {
+            if (event.target.readyState === 4) {
+                if (event.target.status === 200) {
+                    var res = JSON.parse(event.target.responseText);
+                    if(res.msg === '') {
+                        var inputs = document.getElementById("addUser").getElementsByTagName("input");
+                        for(input in inputs)
+                            if(inputs[input].value !== undefined)
+                                inputs[input].value = "";
+                        populateTable(null);
+                    } else alert('Error: ' + res.msg)
+                } else alert('There was a problem with the request.');
+            }
+        } catch (e) {
+             alert('Caught Exception: ' + e.description);
+        }
+    }
+
+    function deleteUser (event) {
+        event.preventDefault();
+        var confirmation = confirm('Are you sure you want to delete this user?');
+        if(confirm)
+            AJAX(remove, 'DELETE', '/users/deleteuser/' + event.target.rel, '');
+    }
+
+    function remove (event) {
+        try {
+            if (event.target.readyState === 4) {
+                if (event.target.status === 200) {
+                    var res = JSON.parse(event.target.responseText);
+                    if(res.msg !== '')
+                        alert('Error: ' + res.msg);
+                    populateTable(null);
+                } else alert('There was a problem with the request.');
+            }
+        } catch (e) {
+             alert('Caught Exception: ' + e.description);
+        }
     }
